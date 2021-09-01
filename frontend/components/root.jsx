df32697d5126fb4da904da7c5a104807192404d4
Muntaqa Maahi <maahi305@yahoo.com>
2021-09-01 02:13:19 -0400

    add sidebar toggle context and change mainpage layout on sidebar toggled

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 
+export const SidebarContext = createContext(null);
+
 const Root = ({ store }) => {
+  const [sidebarExpanded, setSidebarExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setSidebarExpanded(!sidebarExpanded);
+  };
+
   return (
     <Provider store={store}>
       <HashRouter>
-        <App />
+        <SidebarContext.Provider value={{ sidebarExpanded, toggleExpanded }}>
+          <App />
+        </SidebarContext.Provider>
       </HashRouter>
     </Provider>
   );
