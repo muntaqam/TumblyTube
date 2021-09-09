cd418d5c1f8d2f80174da7a068a451d4bd1fdc30
Muntaqa Maahi <maahi305@yahoo.com>
2021-09-09 19:40:50 -0400

    create subscribe button and add to video show

--- frontend/components/show_video/show_video_container.js
+++ frontend/components/show_video/show_video_container.js
@@ -4,12 +4,14 @@ import { connect } from "react-redux";
 import VideoShow from "./show_video";
 import { addViews, fetchVideos } from "../../actions/videos_actions";
 import { fetchComments } from "../../actions/comment_actions";
+import { subscribe, unsubscribe } from "../../actions/subscription_actions";
 
-const mSTP = ({ entities }, ownProps) => {
+const mSTP = ({ entities, session }, ownProps) => {
   return {
     videos: Object.values(entities.videos),
     currentVideo: entities.videos[ownProps.match.params.id],
     currentVideoId: ownProps.match.params.id,
+    currentUserId: session.id,
   };
 };
 
@@ -19,6 +21,8 @@ const mDTP = (dispatch) => {
     fetchVideos: (addViewId) => dispatch(fetchVideos(addViewId)),
     fetchComments: (vidId, numLimit) =>
       dispatch(fetchComments(vidId, numLimit)),
+    // subscribe: (subscription) => dispatch(subscribe(subscription)),
+    // unsubscribe: (subscribeeId) => dispatch(unsubscribe(subscribeeId)),
   };
 };
 
