c9c5ad7d36cc2000ac53c544570deaafa0d876ea
Muntaqa Maahi <maahi305@yahoo.com>
2021-09-11 00:07:18 -0400

    add useHandleClickOutside hook

--- frontend/components/nav_bar/session_button.jsx
+++ frontend/components/nav_bar/session_button.jsx
@@ -1,80 +1,53 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import ExitToAppIcon from "@material-ui/icons/ExitToApp";
-import { avatarFromInitials } from "../../util/avatar_util";
 import AccountCircle from "@material-ui/icons/AccountCircle";
+import { avatarFromInitials } from "../../util/avatar_util";
+import SessionButtonDropDown from "./session_dropdown";
+import { useHandleClickOutside } from "../../hooks/useHandleClickOutside";
 
-class SessionButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showDropDown: false,
-    };
-  }
+const SessionButton = ({ logout, currentUser }) => {
+  const [showDropDown, setShowDropDown] = useState(false);
+  const dropDownRef = useRef();
 
-  handleLogout(e) {
+  useHandleClickOutside({ dropDownRef, showDropDown, setShowDropDown });
+
+  const handleLogout = (e) => {
     e.preventDefault();
-    this.props.logout();
+    logout();
+  };
+
+  let display;
+
+  if (currentUser) {
+    display = (
+      <div className='navbar__session'>
+        <img
+          className='navbar__session__avatar'
+          src={avatarFromInitials(currentUser, 32)}
+          alt='avatar'
+          onClick={() => setShowDropDown(!showDropDown)}
+        />
+
+        {showDropDown && (
+          <div ref={dropDownRef} className='navbar__session__dd'>
+            <SessionButtonDropDown
+              currentUser={currentUser}
+              logout={handleLogout}
+            />
+          </div>
+        )}
+      </div>
+    );
+  } else {
+    display = (
+      <Link to='/login' className='navbar__session__login'>
+        <AccountCircle id='signin-button' />
+        <div className='navbar__session__login__text'>SIGN IN</div>
+      </Link>
+    );
   }
 
-  render() {
-    const { currentUser } = this.props;
-    const { showDropDown } = this.state;
-    let display;
-
-    if (currentUser) {
-      display = (
-        <div className='navbar__session'>
-          <img
-            className='navbar__session__avatar'
-            src={avatarFromInitials(currentUser, 32)}
-            alt='avatar'
-            onClick={() => this.setState({ showDropDown: !showDropDown })}
-          />
-          {showDropDown && (
-            <div className='navbar__session__dd'>
-              <div className='navbar__session__dd__user'>
-                <img
-                  src={avatarFromInitials(currentUser, 50)}
-                  alt='avatar'
-                  className='navbar__session__avatar navbar__session__avatar--dd'
-                />
-                <div className='navbar__session__dd__info'>
-                  <div className='navbar__session__dd__info__name'>
-                    {currentUser.username}
-                  </div>
-                  <div className='navbar__session__dd__info__email'>
-                    {currentUser.email}
-                  </div>
-                </div>
-              </div>
-              <div
-                className='navbar__session__dd__signout'
-                onClick={this.handleLogout.bind(this)}
-              >
-                <ExitToAppIcon
-                  id='signout-button'
-                  className='navbar__icon navbar__icon--signout'
-                />
-                <div className='navbar__session__dd__signout-text'>
-                  Sign out
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-      );
-    } else {
-      display = (
-        <Link to='/login' className='navbar__session__login'>
-          <AccountCircle id='signin-button' />
-          <div className='navbar__session__login__text'>SIGN IN</div>
-        </Link>
-      );
-    }
-
-    return display;
-  }
-}
+  return display;
+};
 
 export default SessionButton;
