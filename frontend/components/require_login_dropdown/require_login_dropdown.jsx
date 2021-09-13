f5c066c54212fb11f248c46fd76e884a3b59b7a2
Muntaqa Maahi <maahi305@yahoo.com>
2021-09-13 14:41:33 -0400

    add requireLoginDD styles

---
+++ frontend/components/require_login_dropdown/require_login_dropdown.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const RequireLoginDD = ({ mode, blockPos = "bottom", leftPos = 0 }) => {
+  const styles = {
+    position: "absolute",
+    width: "378px",
+    height: "175px",
+    top: `${blockPos === "top" ? "0" : null}`,
+    bottom: `${blockPos === "bottom" ? "0" : null}`,
+    left: leftPos,
+  };
+
+  let title;
+  let subtitle;
+  switch (mode) {
+    case "like":
+      title = "Like this video?";
+      subtitle = "Sign in to make your opinion count.";
+      break;
+    case "dislike":
+      title = "Dislike this video?";
+      subtitle = "Sign in to make your opinion count.";
+      break;
+    case "subscribe":
+      title = "Want to subscribe to this channel?";
+      subtitle = "Sign in to subscribe to this channel.";
+      break;
+    case "upload":
+      title = "Want to upload a video?";
+      subtitle = "Sign in to upload your video";
+      break;
+  }
+
+  return (
+    <div style={styles} className='requireLoginDD'>
+      <section className='requireLoginDD__split requireLoginDD__split--top'>
+        <div className='requireLoginDD__title'>{title}</div>
+        <div className='requireLoginDD__subtitle'>{subtitle}</div>
+      </section>
+      <section className='requireLoginDD__split requireLoginDD__split--bottom'>
+        <div className='requireLoginDD__button'>SIGN IN</div>
+      </section>
+    </div>
+  );
+};
+
+export default RequireLoginDD;
