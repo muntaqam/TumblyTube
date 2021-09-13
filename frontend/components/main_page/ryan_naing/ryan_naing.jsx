f108db70978e97f24fb3ff671a3f97babd03a552
Muntaqa Maahi <maahi305@yahoo.com>
2021-09-13 14:41:33 -0400

    add requireLoginDD styles

--- frontend/components/main_page/ryan_naing/ryan_naing.jsx
+++ frontend/components/main_page/ryan_naing/ryan_naing.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import RequireLoginDD from "../../require_login_dropdown/require_login_dropdown";
 
 export default function RyanNaing() {
   return (
     <div className='main__ryan'>
       <h1>RYAN NAING</h1>
+      <RequireLoginDD mode='like' blockPos='0' leftPos='0' />
     </div>
   );
 }
