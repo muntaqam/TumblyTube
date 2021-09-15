eb8a98bccda3b9dcc7308aa819d92466a9ca770b
Muntaqa Maahi <maahi305@yahoo.com>
2021-09-15 14:55:43 -0400

    fix handleClickOutside for sessionbutton dropdown

--- frontend/components/nav_bar/session_button/session_button.jsx
+++ frontend/components/nav_bar/session_button/session_button.jsx
@@ -1,18 +1,11 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { avatarFromInitials } from "../../../util/avatar_util";
 import SessionButtonDropDown from "./session_dropdown";
 import { useHandleClickOutside } from "../../../hooks/useHandleClickOutside";
 
 const SessionButton = ({ logout, currentUser }) => {
-  const dropDownRef = useRef();
-  const [showDropDown, setShowDropDown] = useState(false);
-
-  useHandleClickOutside({ dropDownRef, showDropDown, setShowDropDown });
-
-  const handleClick = (e) => {
-    e.stopPropagation();
-    setShowDropDown(!showDropDown);
-  };
+  const { showDropdown, triggerRef, dropdownRef } =
+    useHandleClickOutside(false);
 
   const handleLogout = (e) => {
     e.preventDefault();
@@ -20,16 +13,17 @@ const SessionButton = ({ logout, currentUser }) => {
   };
 
   return (
-    <div ref={dropDownRef} className='navbar__session'>
+    <div className='navbar__session'>
       <img
+        ref={triggerRef}
         className='navbar__session__avatar'
         src={avatarFromInitials(currentUser, 32)}
         alt='avatar'
-        onClick={handleClick}
       />
 
-      {showDropDown && (
+      {showDropdown && (
         <SessionButtonDropDown
+          ref={dropdownRef}
           currentUser={currentUser}
           logout={handleLogout}
         />
