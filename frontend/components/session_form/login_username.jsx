d5fbabded888666082e3cb622dfa3b8d239bc0c5
Muntaqa Maahi <maahi305@yahoo.com>
2021-09-19 14:52:52 -0400

    add avatar to login identified user

--- frontend/components/session_form/login_username.jsx
+++ frontend/components/session_form/login_username.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import * as EmailValidator from "email-validator";
 
 class LoginUsername extends React.Component {
   constructor(props) {
@@ -39,55 +38,55 @@ class LoginUsername extends React.Component {
     let showErr;
     if (errors.length > 0) {
       showErr = (
-        <div className="session__errors">
-          <span className="material-icons">error</span>
-          <span className="session__errors__type">{errors[0]}</span>
+        <div className='session__errors'>
+          <span className='material-icons'>error</span>
+          <span className='session__errors__type'>{errors[0]}</span>
         </div>
       );
     }
 
     return (
-      <div className="login login--container">
-        <header className="session__header session__header__login">
-          <Link to="/" className="session__header__logo__container">
+      <div className='login login--container'>
+        <header className='session__header session__header__login'>
+          <Link to='/' className='session__header__logo__container'>
             <img
-              className="session__header__logo__img"
+              className='session__header__logo__img'
               src={window.tumbleLogoURL}
             />
           </Link>
-          <h2 className="session__header__title">Sign in</h2>
-          <span className="session__header__subtitle">
+          <h2 className='session__header__title'>Sign in</h2>
+          <span className='session__header__subtitle'>
             to continue to TumblyTube
           </span>
         </header>
-        <form className="login__form">
-          <div className="login__form__input">
+        <form className='login__form'>
+          <div className='login__form__input'>
             <input
               className={
                 "login__form__input__item" +
                 (showErr ? " signup__form__input__item--error" : "")
               }
               onChange={this.handleChange}
-              placeholder=" "
+              placeholder=' '
               value={this.state.inputValue}
             />
-            <label className="login__form__input__label">
+            <label className='login__form__input__label'>
               Email or Username
             </label>
             {showErr}
           </div>
-          <div className="login__form__question">
+          <div className='login__form__question'>
             Don't want to sign in or create an account?{" "}
           </div>
-          <a className="login__form__demo" onClick={this.props.handleDemo}>
+          <a className='login__form__demo' onClick={this.props.handleDemo}>
             Sign in as demo user
           </a>
-          <div className="login__form__btn">
-            <div className="login__form__btn__link--heavy">
+          <div className='login__form__btn'>
+            <div className='login__form__btn__link--heavy'>
               {this.props.formLink}
             </div>
             <button
-              className="login__form__btn__item"
+              className='login__form__btn__item'
               onClick={this.handleNextStep}
             >
               Next
